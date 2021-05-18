50b9ca2eb7cc0c4919074ec16f4335f52ad99ec9
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-17 23:31:57 -0700

    commit tree a3b39eadaa7ddfd0e6a0ef51ee0b5c5ed335fc0d

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      remainingChars: 280,
+    };
+  }
+
+  handleChange = (event) => {
+    debugger
+    this.setState({
+      message: event.target.value,
+      remainingChars: this.props.maxChars - event.target.value.length
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.handleChange(event)} value={this.state.message}/>
+        <p>characters remaining: {this.state.remainingChars}</p>
       </div>
     );
   }
